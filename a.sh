6efcd6645d6e3719c3fb338968bf7c2e402364db
supersketchy <encryptprogramming@gmail.com>
2024-10-28 05:43:02 +0000

    leaderboard update

--- a.sh
+++ a.sh
@@ -1,5 +1,9 @@
-cd /home/supersketchy/git/lelandstocks.github.io
-pixi run all
-git add -A .
-git commit -m "leaderboard update"
-git push origin
+while true
+do
+	cd /home/supersketchy/git/lelandstocks.github.io
+	pixi run all
+	git add -A .
+	git commit -m "leaderboard update"
+	git push origin
+	sleep 90
+done
