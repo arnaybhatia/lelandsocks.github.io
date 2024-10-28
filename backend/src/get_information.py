a5924aceb4fe46d6d91f3697640f54ef65a1a999
supersketchy <encryptprogramming@gmail.com>
2024-10-28 06:41:36 +0000

    leaderboard update

--- backend/src/get_information.py
+++ backend/src/get_information.py
@@ -48,7 +48,7 @@ def get_user_account_information():
     INVESTOPEDIA_USER_ID = int(os.environ.get("INVESTOPEDIA_USER_ID"))
     driver.get(r"https://www.investopedia.com/simulator/portfolio")
     time.sleep(10)
-    print(driver.current_url)
+    # print(driver.current_url)
     account_value = driver.find_element(
         By.XPATH, '//div[contains(text(), "Account Value")]/following-sibling::div'
     ).text
@@ -76,9 +76,9 @@ def get_account_information():
             driver.get(
                 rf"{line}"
             )  # what the heck is a french string doing here: https://stackoverflow.com/a/58321139
-            print(driver.current_url)
-            time.sleep(5)
-            print(driver.current_url)
+            # print(driver.current_url)
+            time.sleep(3)
+            # print(driver.current_url)
             account_value = driver.find_element(
                 By.XPATH, '//*[@data-cy="account-value-text"]'
             ).text
