cf886477e62aaeccada01a0ff9744a27d75002e1
supersketchy <encryptprogramming@gmail.com>
2024-10-29 07:13:32 +0000

    leaderboard update

--- backend/src/get_information.py
+++ backend/src/get_information.py
@@ -78,7 +78,7 @@ def get_account_information():
                 rf"{line}"
             )  # what the heck is a french string doing here: https://stackoverflow.com/a/58321139
             # print(driver.current_url)
-            time.sleep(3)
+            time.sleep(2)
             # print(driver.current_url)
             account_value = driver.find_element(
                 By.XPATH, '//*[@data-cy="account-value-text"]'
