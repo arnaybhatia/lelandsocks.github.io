d744e957d2cebdaf921d075bb14e9a9dca7918e9
supersketchy <encryptprogramming@gmail.com>
2024-10-29 17:55:18 +0000

    leaderboard update

--- backend/src/test.py
+++ backend/src/test.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,6 +22,8 @@ def login():
 
 def get_stock_table(url):
     driver.get(url)
+    time.sleep(0.5)
+    print(driver.current_url)
     table = driver.find_element(By.XPATH, '//table')
     rows = table.find_elements(By.TAG_NAME, 'tr')
     
@@ -32,7 +36,26 @@ def get_stock_table(url):
     return stock_data
 
 # --- main ---
-driver = webdriver.Firefox()
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-renderer-backgrounding")
+options.add_argument("--disable-background-timer-throttling")
+options.add_argument("--disable-backgrounding-occluded-windows")
+options.add_argument("--disable-client-side-phishing-detection")
+options.add_argument("--disable-crash-reporter")
+options.add_argument("--disable-oopr-debug-crash-dump")
+options.add_argument("--no-crash-upload")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-low-res-tiling")
+options.add_argument("--log-level=3")
+options.add_argument("--silent")
+driver = webdriver.Chrome(options=options)
+driver.delete_all_cookies()
+
+
 login()
 url = 'https://www.investopedia.com/simulator/games/user-portfolio?portfolio=10445735'
 stock_table = get_stock_table(url)
