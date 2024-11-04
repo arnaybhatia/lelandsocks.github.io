38d2072ff7758309869ebd0b9347abfa77695119
pythoncrazy <encryptprogramming@gmail.com>
2024-11-04 22:02:07 +0000

    leaderboard update

--- backend/src/make_webpage.py
+++ backend/src/make_webpage.py
@@ -194,10 +194,10 @@ def make_user_page(player_name):
             df = df.sort_values(by=["Money In Account"], ascending=False)
             df["Ranking"] = range(1, 1 + len(df))
             rankings.append(
-                df.loc[df["Account Name"] == player_name, "Ranking"].values[0]
+                df.loc[df["Account Name"] == player_name, "Ranking"]
             )
             player_money.append(
-                df.loc[df["Account Name"] == player_name, "Money In Account"].values[0]
+                df.loc[df["Account Name"] == player_name, "Money In Account"]
             )
 
         print(player_money)
