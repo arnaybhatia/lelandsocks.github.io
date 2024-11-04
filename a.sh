c6dc06ec8ae7085b1539f0f2f0eb4c6b0b669ad0
pythoncrazy <encryptprogramming@gmail.com>
2024-11-04 07:49:43 +0000

    leaderboard update

--- a.sh
+++ a.sh
@@ -2,8 +2,4 @@ while true
 do
 	cd /home/supersketchy/git/lelandstocks.github.io
 	pixi run all
-	git add -A .
-	git commit -m "leaderboard update"
-	git push origin
-	rm -rf /tmp/.org.chromium.Chromium.*
 done
