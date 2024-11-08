7ca815bf9c981b8ba1f2a3c06ea9b20d11758087
Arnay <coolarnay@gmail.com>
2024-11-07 22:21:08 -0800

    Made it so that the script only runs 30 mins before and after market opens

--- a.sh
+++ a.sh
@@ -1,6 +1,26 @@
+#!/bin/bash
+
+# Function to check if the current time is within the desired range
+is_market_open() {
+    current_time=$(date -u +%H:%M)
+    echo "Current UTC Time: $current_time"
+    market_open="14:00"
+    market_close="21:30"
+
+    if [[ "$current_time" > "$market_open" && "$current_time" < "$market_close" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 while true
 do
-	cd /home/supersketchy/git/lelandstocks.github.io
-	pixi run all
-	sleep 900
+    if is_market_open; then
+        cd /home/supersketchy/git/lelandstocks.github.io
+        pixi run all
+    else
+        echo "Market Closed! Sleeping for 15 minutes..."
+    fi
+    sleep 900
 done
